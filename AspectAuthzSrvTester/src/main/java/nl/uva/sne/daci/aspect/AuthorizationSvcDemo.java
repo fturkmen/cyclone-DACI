17786d6a5dd7032b993f10309848aa7911091439
turkmenf <fturkmen@uva.nl>
2017-05-10 17:44:12 +0200

    updated test app

--- AspectAuthzSrvTester/src/main/java/nl/uva/sne/daci/aspect/AuthorizationSvcDemo.java
+++ AspectAuthzSrvTester/src/main/java/nl/uva/sne/daci/aspect/AuthorizationSvcDemo.java
@@ -1,6 +1,8 @@
 package nl.uva.sne.daci.aspect;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,10 +11,14 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,6 +27,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class AuthorizationSvcDemo {
 
 
+	static String lalProviderPolicy = "policies/BioinformaticsCyclone.LAL_ProviderPolicySet.xml";
+	static String intertenantPolicy = "policies/BioinformaticsCyclone.IntertenantPolicies.xml";
+	static String intratenantPolicy = "policies/BioinformaticsCyclone.IFB1_Tenant.xml";
+	
 	
 	/*  This application will try to access private data or execute an application
 	 *  on behalf of a user, and authorization service will check whether the user
@@ -33,7 +43,11 @@ public class AuthorizationSvcDemo {
 		
 		/*Add policies: provider, intertenant and intratenant*/
 		
-		 
+		app.createTenant("Bioinformatics_IFB_Tenant1");
+		app.setPolicy(lalProviderPolicy, "providerPolicy");
+		app.setPolicy(intertenantPolicy, "intertenantPolicy");
+		app.setPolicy(intratenantPolicy, "tenantUserPolicy");
+    	
 		
 		
 		/*Check the authorizations ...*/
@@ -43,6 +57,7 @@ public class AuthorizationSvcDemo {
 		else System.out.println("NOT AUTHORIZED!!!");
 	}
 	
+
 	
 	/*This will be direct check, call the service...*/
 	public boolean readPrivateData_Integrated(AuthzRequest ar, String tenantId) throws Exception{
@@ -93,6 +108,86 @@ public class AuthorizationSvcDemo {
 		attrs.put(ACTION_ID, actionId);
 		request.setAttributes(attrs);
 		return request;
+	}
+	
+	
+	
+	
+	private void  setPolicy(String policyFile, String endPoint) throws Exception {
+		
+		String output = null;
+        String url = "http://localhost:8092/" + endPoint;
+        HttpClient client = HttpClientBuilder.create().build();
+        //ObjectMapper mapper = new ObjectMapper();
+        try{
+            HttpPost mPost = new HttpPost(url);
+  
+            //mPost.setHeader("Content-Type", "application/xml");
+            //mPost.setHeader("accept", "application/xml");
+            
+            mPost.setEntity(new StringEntity("localhost"));
+            mPost.setEntity(new StringEntity("demo-uva"));
+            
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            File f = new File(policyFile);
+            builder.addBinaryBody(
+            	    "policy",
+            	    new FileInputStream(f),
+            	    ContentType.APPLICATION_OCTET_STREAM,
+            	    f.getName()
+            	);
+
+            HttpEntity multipart = builder.build();
+            mPost.setEntity(multipart);
+            	
+            //FileEntity entity = new FileEntity(new File(policyFile));
+            //entity.setContentType(ContentType.APPLICATION_XML.getMimeType());
+            //mPost.setEntity(entity);      
+
+            HttpResponse response = client.execute(mPost); 
+            
+            output = response.toString();
+            mPost.releaseConnection( );
+            System.out.println("Response : " + output);
+            
+        }catch(Exception e){
+        	throw new Exception("Exception in adding bucket : " + e.getMessage());
+        	
+        }	
+	}
+
+
+
+
+	private void  createTenant(String tenantId) throws Exception {
+		
+		String output = null;
+        String url = "http://localhost:8092/tenants";
+        HttpClient client = HttpClientBuilder.create().build();
+        //ObjectMapper mapper = new ObjectMapper();
+        try{
+            HttpPut mPut = new HttpPut(url);
+              
+            mPut.setHeader("Content-Type", "application/json");
+            //mPut.setHeader("accept", "application/json");
+            
+            mPut.setEntity(new StringEntity("localhost"));
+            mPut.setEntity(new StringEntity("demo-uva"));
+            mPut.setEntity(new StringEntity(tenantId));
+            //FileEntity entity = new FileEntity(new File(policyFile));
+            //entity.setContentType(ContentType.APPLICATION_XML.getMimeType());
+            //mPost.setEntity(entity);      
+
+            HttpResponse response = client.execute(mPut); 
+            
+            output = response.toString();
+            mPut.releaseConnection( );
+            System.out.println("Response... : " + output);
+            
+        }catch(Exception e){
+        	throw new Exception("Exception in adding bucket : " + e.getMessage());
+        	
+        }	
 	}
 	
 	
