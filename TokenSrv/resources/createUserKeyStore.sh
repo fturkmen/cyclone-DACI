f5bec3fdbd787f1703cd9217a7fab4af28278d4c
turkmenf <fturkmen@uva.nl>
2017-10-16 12:12:16 +0200

    certificate

--- TokenSrv/resources/createUserKeyStore.sh
+++ TokenSrv/resources/createUserKeyStore.sh
@@ -14,6 +14,6 @@ VALIDITY=365
 
 KEYSIZE=2048
 
-keytool -genkey -alias $ALIAS -dname "CN=CanhNgo, OU=SNE Group, O=UvA, C=NL" -validity $VALIDITY -keypass $KEYPASS -keyalg "RSA" -keysize $KEYSIZE -keystore $KEYSTORE -storepass $STOREPASS -storetype $STORETYPE
+keytool -genkey -alias $ALIAS -dname "CN=UvAUser, OU=SNE Group, O=UvA, C=NL" -validity $VALIDITY -keypass $KEYPASS -keyalg "RSA" -keysize $KEYSIZE -keystore $KEYSTORE -storepass $STOREPASS -storetype $STORETYPE
 
 keytool -exportcert -file "$ALIAS.crt" -keystore $KEYSTORE -storepass $STOREPASS -alias $ALIAS -rfc
